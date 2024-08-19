a8060907e15670c704b1e803aab6d3e23975fb33
Alishak003 <125250147+Alishak003@users.noreply.github.com>
2024-08-19 16:09:21 +0530

    Update main.js

--- modules/main.js
+++ modules/main.js
@@ -23,7 +23,7 @@ function addCard() {
   card.style.scale = 2.65;
   card.style.transform = "rotate(0deg)";
   card.style.right = cardRight + "px";
-  card.style.bottom = cardBottom + "px";
+  card.style.top = 35 + "%";
   card.classList.add("card--loaded");
   card.classList.remove("hover-effect");
   body.style.height = 100 + "vh";
@@ -35,9 +35,44 @@ function removeCard() {
   card.style.scale = 1;
   card.style.transform = "rotate(20deg)";
   card.style.right = cardStyle.getPropertyValue("right");
-  card.style.bottom = cardStyle.getPropertyValue("bottom");
+  card.style.top = "auto";
   card.classList.add("hover-effect");
   card.classList.remove("card--loaded");
   body.style.height = "auto";
   body.style.overflowY = "scroll";
 }
+
+// magentic effect
+
+let magnets = document.querySelectorAll(".magnetic");
+
+magnets.forEach((ele) => {
+  let boundingRect = ele.getBoundingClientRect();
+
+  window.addEventListener("resize", () => {
+    boundingRect = ele.getBoundingClientRect();
+  });
+
+  ele.addEventListener("mousemove", (e) => {
+    const mousePosX = e.pageX - boundingRect.left;
+    const mousePosY = e.pageY - boundingRect.top;
+
+    gsap.to(ele, {
+      x: (mousePosX - boundingRect.width / 2) * 0.4,
+      y: (mousePosY - boundingRect.height / 2) * 0.4,
+      duration: 0.8,
+      ease: "power3.out",
+    });
+
+    gsap.to(ele, {});
+  });
+
+  ele.addEventListener("mouseleave", () => {
+    gsap.to(ele, {
+      x: 0,
+      y: 0,
+      duration: 0.8,
+      ease: "elastic.out(1,0.3)",
+    });
+  });
+});
